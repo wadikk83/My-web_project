6da838b5edf2d0ecba5ed6b1a7833fe1c0a32f38
wadikk83 <wadikk83@gmail.com>
2020-09-08 18:45:20 +0300

    don't work mvc viewer

--- webapp/src/main/java/by/wadikk/config/MvcConfig.java
+++ webapp/src/main/java/by/wadikk/config/MvcConfig.java
@@ -10,11 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("index");
-	}
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/templates/**");
-	}
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/templates/**");
+    }
 }
